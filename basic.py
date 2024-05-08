6d179a563e568aee906eb12523bc70939a21bb63
Aisha <workaisha24@gmail.com>
2024-05-08 19:30:11 +0530

    hello

---
+++ basic.py
@@ -0,0 +1,10 @@
+import pytesseract as pyt
+import cv2
+
+img = cv2.imread("text_img.png")
+
+pyt.pytesseract.tesseract_cmd = "C:\\Users\\worka\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe"
+
+text  = pyt.image_to_string(img)
+
+print(text)
